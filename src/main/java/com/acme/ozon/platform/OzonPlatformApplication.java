6d7c224db55a538a0c82e0f37f553f2c943ed12d
Oscar <oscaredu01@hotmail.com>
2024-12-07 14:51:22 -0500

    chore: initial commit

---
+++ src/main/java/com/acme/ozon/platform/OzonPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.ozon.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzonPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OzonPlatformApplication.class, args);
+    }
+
+}
